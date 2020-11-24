a5ad3551dc7da7b339ba02065468e8f1a85ca3b2
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2020-11-24 14:53:54 +0100

    removed proxy configuration for cors, changed api url in environments for login and signup, added encryption to signup request data

---
+++ src/app/services/authentication/authentication.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json; charset=utf-8",
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+
+    Authenticate(request: any): Observable<any> {
+    return this.http
+      .post<any>(
+        environment.enviromentURL + environment.authenticationEndPoint,
+        request,
+        httpOptions
+      )
+      .pipe
+      //  catchError(this.handleError('signup', request))
+      ();
+  }
+
+}
