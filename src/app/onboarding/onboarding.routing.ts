e84680f9521abb920599b496baccfe2b2cc73d00
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2021-03-01 16:24:15 +0100

    commit to pull recent changes

--- src/app/onboarding/onboarding.routing.ts
+++ src/app/onboarding/onboarding.routing.ts
@@ -33,7 +33,7 @@ const routes :Routes = [
       },
       {
         path: 'dashboard',
-        canActivate: [AuthGuard],
+        // canActivate: [AuthGuard],
         loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
       }
     ]
